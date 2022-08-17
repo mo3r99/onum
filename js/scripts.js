3568309b58550532994f0bd4978670bc7580ede2
mo3r99 <mo3r99@gmail.com>
2022-08-17 22:22:31 +0100

    modal on every page

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,30 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 1500);
 });
 
+const modal = document.querySelector('.modal');
+
+function showModal() {
+    modal.style.display="none";
+    gsap.to(modal, {x: 100, duration: 0, opacity: 0});
+    modal.style.display="flex";
+    gsap.to(modal, {x: 0, duration: 0.4, opacity: 1});
+}
+
+ScrollTrigger.create({
+    trigger: ".modal-trigger",
+    start: "center center",
+    onEnter: self => showModal(),
+    once: true
+});
+
+function hideModal() {
+    gsap.to(modal, {opacity: 0, duration: 0.4});
+
+    window.setTimeout(function () {
+        modal.style.display = 'none';
+    }, 500);
+}
+
 
 (function( $ ) {
     "use strict";
