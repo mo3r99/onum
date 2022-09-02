2903f09f59e345a266f8aa5d875a25049f481a7c
mo3r99 <mo3r99@gmail.com>
2022-09-02 18:00:48 +0100

    final edits

--- js/readmore.js
+++ js/readmore.js
@@ -1,1 +1,8 @@
-$('.author-widget_text').readmore({ speed: 175, lessLink: '<a href="#" style="color: white; text-transform: uppercase; margin-bottom: 10px">Read less</a>', moreLink:  '<a href="#" style="color: white; text-transform: uppercase; margin-bottom: 10px">Read More</a>', collapsedHeight: 60});
+(function( $ ) {
+    $('.author-widget_text').readmore({ 
+        speed: 175, 
+        lessLink: '<a href="#" style="color: white; text-transform: uppercase; margin-bottom: 10px">Read less</a>', 
+        moreLink:  '<a href="#" style="color: white; text-transform: uppercase; margin-bottom: 10px">Read More</a>', 
+        collapsedHeight: 60
+    });
+})
